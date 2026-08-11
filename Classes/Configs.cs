7fe9809093eeb617b9c9e6987f5fa66bd903e368
dom <166765848+xfi0@users.noreply.github.com>
2026-08-11 08:30:42 -0500

    feat: publish workflow.
    chore: bump config version.
    
    refactor: rename ci workflow to ci from dotnet.

--- Classes/Configs.cs
+++ Classes/Configs.cs
@@ -10,7 +10,7 @@ namespace Titled_Gui.Classes
     internal class Configs : Classes.ThreadService
     {
         public static string MenuName = "Titled";
-        public static string Version = "2.3";
+        public static string Version = "2.3.1";
         public static string Author = "https://github.com/xfi0";
         public static string Link = "https://github.com/xfi0/Titled-Gui-CS2";
         public static string titledDocumentsFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Titled", "CS2", "External");
